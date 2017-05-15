256247e3f0d6557b8004f27298fa06e811e9be7b
ali <ali.barhmouni@hotmail.com>
2017-05-15 19:20:30 +0200

    added img

--- script/main.js
+++ script/main.js
@@ -1,15 +1,16 @@
-var game = new Phaser.Game(600,190,Phaser.AUTO,'game',
+var game = new Phaser.Game(1800,920,Phaser.AUTO,'game',
 		{preload:preload,create:create,update:update,render:render});
 
 function preload(){
 	
-
+	game.load.image('circle', 'img/test.png');
 };
 
 
 function create(){
 	
-
+	var i = game.add.image(290,290,'circle');
+    i.anchor.set(0.5);
 };
 
 
