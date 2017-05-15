69f4a0e139f2c536699c2807192a9ca19deeae5e
ali <ali.barhmouni@hotmail.com>
2017-05-15 14:57:55 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,24 @@
+var game = new Phaser.Game(600,190,Phaser.AUTO,'game',
+		{preload:preload,create:create,update:update,render:render});
+
+function preload(){
+	
+
+};
+
+
+function create(){
+	
+
+};
+
+
+function update(){
+	
+
+};
+
+function render(){
+	
+
+};
