8f6012b592b4fea463ea1d94e393473eea8d6ca8
ali <ali.barhmouni@hotmail.com>
2017-05-16 17:57:38 +0200

    adding animations

--- script/main.js
+++ script/main.js
@@ -7,13 +7,16 @@ function preload(){
 	game.load.spritesheet('eatMan', 'img/meatSpriteSheet.png', 80, 110, 28);
 };
 var eatMan;
-
+var isWalking = false;
+var isDown = false;
 
 function create(){
+
 	
 	eatMan = game.add.sprite(320,770,'eatMan');
     eatMan.anchor.set(0.5);
-    var walk = eatMan.animations.add('walkRight', [0,3], 10, true);
+    var walk = eatMan.animations.add('walkRight', [0,1,2,3], 10, true);
+    var getDown = eatMan.animations.add('getDown', [4,6], 5, true);
     eatMan.animations.play('walk');
     game.physics.enable(eatMan, Phaser.Physics.ARCADE);
     cursors = game.input.keyboard.createCursorKeys();
@@ -31,6 +34,7 @@ function update(){
     }
     else if (cursors.right.isDown)
     {
+
         eatMan.body.velocity.x = 100;
         eatMan.play('walkRight');
     }
@@ -43,6 +47,15 @@ function update(){
     {
         // eatMan.body.velocity.y = 100;
         // eatMan.play('down');
+        if(!isDown){
+        	isDown = true;
+        	if(isDown)
+        	{
+        		eatMan.play('getDown');
+        	}
+        	
+        	isDown = false;
+        }
     }
     else
     {
