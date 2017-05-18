611727ab7f4cbe779863c91a4dae29451da196fa
ali <ali.barhmouni@hotmail.com>
2017-05-18 13:32:23 +0200

    created platforms and weapons

--- script/main.js
+++ script/main.js
@@ -6,8 +6,11 @@ function preload(){
 	// game.load.image('circle', 'img/meatBoy.png');
     game.load.tilemap('map', 'map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tileMap', 'img/map.png');
+    game.load.image('platform', 'img/platform.png');
+    game.load.image('plante1', 'img/plants1.png');
+    game.load.image('plante2', 'img/plants2.png');
     game.load.spritesheet('eatMan', 'img/meatSpriteSheet.png', 80, 110, 28);
-	game.load.spritesheet('bullet', 'img/bullet.png', 96, 33, 3);
+	game.load.spritesheet('bullet', 'img/meatBullet.png', 10, 10, 1);
     game.load.spritesheet('flowerSprite', 'img/flowerSprite.png', 192/3, 64, 3);
 
 };
@@ -25,32 +28,38 @@ var weapon;
 function create(){
 	map = game.add.tilemap('map');
     map.addTilesetImage('tileset','tileMap');
+    map.addTilesetImage('platform','platform');
     game.stage.backgroundColor = 'blue';
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.gravity.y = 6000;
+   
 
     layer = map.createLayer('fond');
     layerCollision = map.createLayer('collisions');
     layerCollision.alpha = 0;
-    map.setCollisionBetween(0, 22,true,layerCollision);
+    map.setCollisionBetween(0, 12,true,layerCollision);
+    game.physics.startSystem(Phaser.Physics.arcade);
 	eatMan = game.add.sprite(320,770,'eatMan');
+	plante1 = game.add.sprite(500,805,'plante1');
+	plante2 = game.add.sprite(400,805,'plante2');
     
-    eatMan.anchor.set(0.5);
+   
     eatMan.animations.add('walk', [0,1,2,3], 10, true);
-    game.physics.enable(eatMan, Phaser.Physics.ARCADE);
-    // eatMan.body.gravity.set(0,600);
+    game.physics.arcade.enable( eatMan );
+    eatMan.body.gravity.set(0, 600);
+    eatMan.anchor.set(0.5);
+    game.camera.follow(eatMan);
 
+    game.world.setBounds(0, 0, 71680, 896);
     weapon = game.add.weapon(10, 'bullet');
-    // weapon.setBulletFrames(0, 0, true);
-    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-    weapon.bulletSpeed = 200;
-    weapon.fireRate = 300;
-    weapon.trackSprite(eatMan, 70, 0, true);
+    weapon.setBulletFrames(0, 0, true);
+    weapon.bulletKillType = Phaser.Weapon.KILL_CAMERA_BOUNDS;
+    weapon.bulletSpeed = 500;
+    weapon.fireRate = 500;
+    weapon.trackSprite(eatMan, 20, 15, true);
 
 
       /* Flower animations */
     flower = game.add.sprite(320,805,'flowerSprite');
-    flower.animations.add('danse', [0,1,2], 2, true);
+    flower.animations.add('danse', [0,1,2,1], 3, true);
     flower.animations.play('danse');
 
 
@@ -61,9 +70,8 @@ function create(){
     idle = eatMan.animations.add('idle', [8,9], 5, true);
     eatMan.animations.add('walk', [0,1,2,3], 10, true);
     eatMan.animations.add('getDown', [6], 5, true);
-    eatMan.animations.add('fire', [10,11], 5, true);
+    eatMan.animations.add('fire', [5,11], 5, true);
     eatMan.animations.add('jump', [10,11], 5, true);
-    eatMan.animations.play('walk');
    
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -74,7 +82,7 @@ function update(){
 
     game.physics.arcade.collide(layerCollision, eatMan);
 
-	eatMan.body.velocity.set(0);
+	eatMan.body.velocity.x = 0;
 	console.log(idle);
 	if(idle)
 	{
@@ -195,9 +203,9 @@ function update(){
          if (jumpButton.isDown && game.time.now > jumpTimer)
          {
             
-             
-                eatMan.body.velocity.y = -6000;
+                eatMan.body.velocity.y = -400;
                 jumpTimer = game.time.now + 750;
+                
          }
      }
 
